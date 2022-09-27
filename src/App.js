4bb6444f3261989f1c443788e58c27b65efad1a9
Hamaddogar <hamadali77115033@gmail.com>
2022-09-26 22:36:23 -0700

    test

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component }  from 'react';
+// import Header from "./Home/Header/Header"
+//  import Body from "./Home/body/body"
+//   import Footer from "./Home/footer/footer"
+ import Dashboard  from './Dashboard/dashboard';
+const App = () => {
+  return (
+    <div> 
+        <Dashboard/>
+  {/* <Header/>
+   <Body/>
+   <Footer/> */}
+
+    </div>
+  )
+
+
+}
+
+export default App
